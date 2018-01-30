40286cf73f6c9c59f1dced22f5d2c0dee05c7769
himanshu.shekhar <himanshu.shekhar@power2sme.com>
2018-01-30 12:57:51 +0530

    changed admin ui path

--- springbootadmin/src/main/java/com/power2sme/springboot/Application.java
+++ springbootadmin/src/main/java/com/power2sme/springboot/Application.java
@@ -2,15 +2,29 @@ package com.power2sme.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-	public static void main(String args[]) {		
-		System.setProperty("spring.application.name", "HIM-SPRINGBOOT APP");
+	
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    		configureSystemProperties();
+        return application.sources(Application.class);
+    }
+	
+	public static void main(String args[]) {	
+		configureSystemProperties();
 		SpringApplication.run(Application.class, args);
 	}	
+	
+	private static void configureSystemProperties() {
+		System.setProperty("spring.application.name", "HIM-SPRINGBOOT APP");
+		System.setProperty("spring.boot.admin.context-path", "/admin");
+	}
+	
 }
