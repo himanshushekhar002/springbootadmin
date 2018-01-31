ef9ffe361d741bd704877b42deb2f019648d70c0
himanshu.shekhar <himanshu.shekhar@power2sme.com>
2018-01-31 13:04:50 +0530

    changed app controller name ; removed extra content from pom

--- springbootadmin/src/main/java/com/power2sme/springboot/rest/AppController.java
+++ springbootadmin/src/main/java/com/power2sme/springboot/rest/AppController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/application")
-public class AppNameController {
+@RequestMapping("/app")
+public class AppController {
 
 	@Value("${spring.application.name}")
 	private String appName;
