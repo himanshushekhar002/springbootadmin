761520b42aa3735a2d1b39e26cfaa7a6f010860c
himanshu.shekhar <himanshu.shekhar@power2sme.com>
2018-03-06 12:02:37 +0530

    removed unnecesaary files

--- springbootadmin/src/main/java/com/power2sme/springboot/config/CustomConfiguration.java
+++ springbootadmin/src/main/java/com/power2sme/springboot/config/CustomConfiguration.java
@@ -2,6 +2,12 @@ package com.power2sme.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Use this class for any of the custom project related configuration
+ * This class will be loaded at the time of bootstrap as a configuration
+ * @author himanshuraghuvanshi
+ *
+ */
 @Configuration
 public class CustomConfiguration{
 
